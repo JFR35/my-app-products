88dd751765afbf5ffa8d4f3eadb294b09f2e7a08
Juan <juanfajardoreyes@hotmail.com>
2025-02-28 10:59:11 +0100

    npm Bootstrap Add, tailwind del

--- app/Http/Controllers/Producto.php
+++ app/Http/Controllers/Producto.php
@@ -1,13 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\ProductoModel;
 use Illuminate\Http\Request;
 
 class Producto extends Controller
 {
     public function index()
     {
+        $productos = ProductoModel::all();
         return view('productos.index', compact('productos'));
+    }
+
+    public function show($id)
+    {
+        return view('productos.show', compact('producto'));
     }
 }
