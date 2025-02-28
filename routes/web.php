b01df5271be9d576b7c084f32162ec066e62056d
Juan <juanfajardoreyes@hotmail.com>
2025-02-28 10:43:38 +0100

    Controlador y modelo de Producto y categoria

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Producto;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/productos', [Producto::class, 'index'])->name('producto');
